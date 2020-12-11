35d18b791beeef8a8b17caaf99c8d46dbaab1aaa
zhangquan <zhangquan@medlinker.com>
2020-12-11 13:58:01 +0800

    update

--- fragment/src/main/java/com/review/fragment/adapter/AdapterAct.java
+++ fragment/src/main/java/com/review/fragment/adapter/AdapterAct.java
@@ -3,9 +3,13 @@ package com.review.fragment.adapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.ListFragment;
 
 import com.review.fragment.R;
 
@@ -13,21 +17,37 @@ import com.review.fragment.R;
  * @author zhangquan
  */
 public class AdapterAct extends AppCompatActivity {
+    private static String[] mTitles = new String[]{
+            FragmentPagerAdapterAct.class.getSimpleName(),
+            FragmentStatePagerAdapterAct.class.getSimpleName(),
+            TabAct.class.getSimpleName()
+    };
+
+    private static Class[] mActivities = new Class[]{
+            FragmentPagerAdapterAct.class,
+            FragmentStatePagerAdapterAct.class,
+            TabAct.class
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adapter);
-    }
 
-    public void pagerAdapter(View view) {
-        startActivity(new Intent(this, FragmentPagerAdapterAct.class));
-    }
+        getSupportFragmentManager().beginTransaction().add(R.id.container,new MyListFrag()).commit();
 
-    public void statePagerAdapter(View view) {
-        startActivity(new Intent(this, FragmentStatePagerAdapterAct.class));
     }
+    public static class MyListFrag extends ListFragment {
 
-    public void tabActivity(View view) {
-        startActivity(new Intent(this, TabAct.class));
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            setListAdapter(new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, mTitles));
+        }
+
+        @Override
+        public void onListItemClick(ListView l, View v, int position, long id) {
+            startActivity(new Intent(getContext(), mActivities[position]));
+        }
     }
 }
